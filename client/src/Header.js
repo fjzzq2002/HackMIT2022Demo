d926f03e869cf9f6c1c599d62ab3963c7d33fa8e
fjzzq2002 <fjzzq2002@users.noreply.github.com>
2022-10-01 12:02:01 -0400

    bootstrap

---
+++ client/src/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './index.css';
+
+export default function Home() {
+  return (
+    <>
+      <div className="fullw">
+        <div style={{display:"flex"}}>
+        </div>
+      </div>
+    </>
+  );
+}
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Home from "./Home";
+import Header from "./Header";
 import reportWebVitals from "./reportWebVitals";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import {
   createBrowserRouter,
@@ -20,6 +22,9 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <div className="container">
+    <Header />
     <RouterProvider router={router} />
+    </div>
   </React.StrictMode>
 );
