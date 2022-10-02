5e22f3598e6e3684ac1ee44a4d9e5f3e5681bfc4
Mingyang Deng <peterson11@163.com>
2022-10-02 01:59:54 -0400

    trycatch

--- index.js
+++ index.js
@@ -27,114 +27,152 @@ const { get } = require('http');
 mongoose.connect(mongoURL, {useNewUrlParser: true, useUnifiedTopology: true, dbName: "HackMIT"}).then(() => {console.log("sucessfully connected to database")}).catch((err) => {console.log(err)});
 
 async function verify(username, password) {
-    const user = await User.findOne({ username: username });
-    if (!user) return false;
-    if (user.password == password) 
-        return true;
-    return false;
+    try {
+        const user = await User.findOne({ username: username });
+        if (!user) return false;
+        if (user.password == password) 
+            return true;
+        return false;
+    }
+    catch (err) {
+        console.log(err);
+        return false;
+    }
 }
 
 async function chargeUser(username, cost) {
-	const user = await User.findOne({ username: username });
-    if (!user) return false;
-	if (user.coins < cost) return false;
-	user.coins -= cost;
-	user.save();
-	return true;
+    try {
+        const user = await User.findOne({ username: username });
+        if (!user) return false;
+        if (user.coins < cost) return false;
+        user.coins -= cost;
+        user.save();
+        return true;
+    }
+    catch (err) {
+        console.log(err);
+        return false;
+    }
 }
 async function verifyCookie(req) {
     //console.log(req);
-    let username = req.query.loginname;
-    let password = req.query.password;
-    const user = await User.findOne({ username: username });
-    if (!user) return false;
-    if (user.password === password) {
-        // get date
-        let date = new Date();
-        let lastUpdate = user.lastUpdate;
-        let sameDay = false;
-        if (date.getDate() === lastUpdate.getDate() && date.getMonth() === lastUpdate.getMonth() && date.getFullYear() === lastUpdate.getFullYear()) 
-            sameDay = true;
-        if (!sameDay) {
-            user.coins += 1;
-            user.lastUpdate = date;
-            user.save();
+    try {
+        let username = req.query.loginname;
+        let password = req.query.password;
+        const user = await User.findOne({ username: username });
+        if (!user) return false;
+        if (user.password === password) {
+            // get date
+            let date = new Date();
+            let lastUpdate = user.lastUpdate;
+            let sameDay = false;
+            if (date.getDate() === lastUpdate.getDate() && date.getMonth() === lastUpdate.getMonth() && date.getFullYear() === lastUpdate.getFullYear()) 
+                sameDay = true;
+            if (!sameDay) {
+                user.coins += 1;
+                user.lastUpdate = date;
+                user.save();
+            }
+            return true;
         }
-        return true;
+        else return false;
     }
-    else return false;
+    catch (err) {
+        console.log(err);
+        return false;
+    }
 }
 
 
 async function postArticle(res, req, title, content, description, type) {
-    const newId = Math.floor(Math.random() * 1000000);
-    const author = req.query.loginname;
-	const article = new Article({
-		id: newId,
-		title: title,
-		content: content,
-        description: description, 
-		votes: {
-			upvotes: 0,
-			downvotes: 0,
-			clicks: 0,
-		},
-		author: author,
-        time: new Date(),
-        type: type
-	});
-	article
-		.save()
-		.then((result) => {
-			console.log(result);
-		})
-		.catch((err) => {
-			console.log(err);
-		});
-    const user = await User.findOne({ username: author });
-    user.articles.push({article: newId, cost: -1, shared: true});
-    user.save();
-    res.send(""+newId);
+    try {    
+        const newId = Math.floor(Math.random() * 1000000);
+        const author = req.query.loginname;
+        const article = new Article({
+            id: newId,
+            title: title,
+            content: content,
+            description: description, 
+            votes: {
+                upvotes: 0,
+                downvotes: 0,
+                clicks: 0,
+            },
+            author: author,
+            time: new Date(),
+            type: type
+        });
+        article
+            .save()
+            .then((result) => {
+                console.log(result);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+        const user = await User.findOne({ username: author });
+        user.articles.push({article: newId, cost: -1, shared: true});
+        user.save();
+        res.send(""+newId);
+    }
+    catch (err) {
+        console.log(err);
+    }
 }
 
 async function fetchArticle(id) {
-	const article = await Article.findOne({ id: id });
-	return article;
+    try {
+        const article = await Article.findOne({ id: id });
+        return article;
+    }
+    catch (err) {
+        console.log(err);
+    }
 }
 
 app.get("/api/login", async (req, res) => {
-    const match = await verify(req.query.username, req.query.password);
-    if (match) {
-        res.cookie(`username`, req.query.username);
-        res.cookie(`password`, req.query.password);
-        res.send("Login successful");
+    try {
+        const match = await verify(req.query.username, req.query.password);
+        if (match) {
+            res.cookie(`username`, req.query.username);
+            res.cookie(`password`, req.query.password);
+            res.send("Login successful");
+        }
+        else 
+            res.send("Login failed");
     }
-    else 
-        res.send("Login failed");
+    catch (err) {
+        console.log(err);
+    }
 });
 // create a user
 
 app.get('/api/createUser', async (req, res) => {
     // generate a random number
     // check if the username exists
-    const result = await User.findOne({username: req.query.username});
-    if (result) {
-        res.send("username already exists");
-        return ;
+    try {
+        const result = await User.findOne({username: req.query.username});
+        if (result) {
+            res.send("username already exists");
+            return ;
+        }
+        const user = new User({
+            username: req.query.username,
+            password: req.query.password,
+            coins: 1,
+            articles: [], 
+            lastUpdate: new Date(),
+        });
+        user.save().then((result) => {
+            res.send("Created user successfully");
+        }).catch((err) => {
+            res.send("Failed to create user");
+            console.log(err);
+        });
     }
-    const user = new User({
-        username: req.query.username,
-        password: req.query.password,
-        coins: 1,
-        articles: [], 
-        lastUpdate: new Date(),
-    });
-    user.save().then((result) => {
-        res.send("Created user successfully");
-    }).catch((err) => {
-        res.send("Failed to create user");
+    catch (err) {
         console.log(err);
-    });
+    }
 });
 
 /**
@@ -142,13 +180,20 @@ app.get('/api/createUser', async (req, res) => {
 ​		Input: req.query.username
 ​		Output: {cost: number, lastUpdate: date, articles: [{article: Number, cost: Number, shared: Boolean}]}
  */
-app.get('/api/getInfo', (req, res) => {
-    User.findOne({username: req.query.username}).then((result) => {
-        res.send({coins: result.coins, lastUpdate: result.lastUpdate, articles: result.articles});
-    }).catch((err) => {
+app.get('/api/getInfo', async (req, res) => {
+    try {
+        let result = await User.findOne({ username: req.query.username });
+            
+        res.send({
+            coins: result.coins,
+            lastUpdate: result.lastUpdate,
+            articles: result.articles,
+        });
+    }
+    catch (err) {
         console.log(err);
         res.status(500).send("No such user");
-    });
+    }
 });
 
 /**
@@ -157,14 +202,10 @@ app.get('/api/getInfo', (req, res) => {
 ​		Output: [{article: number, title: string, description: string, votes: {upvotes: number, downvotes: number, clicks: number}, author:string, time: date, type: string}]
  */
 
-app.get("/api/list", (req, res) => {
-    if (!verifyCookie(req)) {
-        res.send("Please log in!");
-        return ;
-    }
-    // find all articles
-    Article.find().then((result) => {
-        // for each, remove the content
+app.get("/api/list", async (req, res) => {
+    try {
+        // find all articles
+        let result = await Article.find();
         res.send(result.map((article) => {
             return {
                 article: article.id,
@@ -176,7 +217,10 @@ app.get("/api/list", (req, res) => {
                 type: article.type,
             }
         }));
-    });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Error");
+    }
 });
 
 /** 
@@ -187,20 +231,32 @@ app.get("/api/list", (req, res) => {
 app.post('/api/post', async (req, res) => {
 
     // get body of request
-    const title = req.body.title;
-    if (! await verifyCookie(req)) {
-        res.send("Not logged in");
-        return ;
+    try {
+        const title = req.body.title;
+        if (! await verifyCookie(req)) {
+            res.send("Not logged in");
+            return ;
+        }
+        const result = req.body;
+        return await postArticle(res, req, result.title, result.content, result.description, result.type);
     }
-    const result = req.body;
-    return await postArticle(res, req, result.title, result.content, result.description, result.type);
+    catch (err) {
+        console.log("Failed to post article");
+        console.log(err);
+    }
 });
 
 function getAccess(user, article) {
-    for (history of user.articles)
-		if (history.article == article) 
-            return history;
-    return null;
+    try {
+        for (history of user.articles)
+            if (history.article == article) 
+                return history;
+        return null;
+    }
+    catch (err) {
+        console.log(err);
+        return null;
+    }
 }
 function haveAccess(user, article) { // user: a real user!
     if (getAccess(user, article) === null) return false;
@@ -211,63 +267,71 @@ function haveAccess(user, article) { // user: a real user!
 ​		Input: req.query.id
 ​		Effect: will buy the article*/
 app.get('/api/buy', async (req, res) => {
-    if (! await verifyCookie(req)) {
-        res.send("Not logged in");
-        return ;
-    }
-    let user = await User.findOne({username: req.query.loginname});
-    console.log("NOW ");
-    if (haveAccess(user, req.query.id)) {
-        res.send("You already have access to this article");
-        return ;
-    }
-    if (! await chargeUser(req.query.loginname, 1)) {
-        res.send("Not enough coins");
-        console.log("money ");
-        return ;
-    }
+    try {
+        if (! await verifyCookie(req)) {
+            res.send("Not logged in");
+            return ;
+        }
+        let user = await User.findOne({username: req.query.loginname});
+        console.log("NOW ");
+        if (haveAccess(user, req.query.id)) {
+            res.send("You already have access to this article");
+            return ;
+        }
+        if (! await chargeUser(req.query.loginname, 1)) {
+            res.send("Not enough coins");
+            console.log("money ");
+            return ;
+        }
 
-    console.log("Bought ");
-    // append the new article
-    user.articles.push({article: req.query.id, cost: 1, shared: false});
-    user.save();
-    res.send('successully bought');
+        console.log("Bought ");
+        // append the new article
+        user.articles.push({article: req.query.id, cost: 1, shared: false});
+        user.save();
+        res.send('successully bought');
+    } catch (err) {
+        console.log(err);
+    }
 });
 // /api/fetch:
 // ​		Input: req.query.id
 // ​		Output: {article: number, title: string,  description: string, **content: string**, votes: {upvotes: number, downvotes: number, clicks: number}, author:string, time: date, type: string}
 
 app.get("/api/fetch", async (req, res) => {
-    console.log("FETCH", req.query);
-    let article = await fetchArticle(req.query.id);
-    if(article==null) {
-        res.send({
-            reason: "Article not found.",
-        });
-        return ;
-    }
-    let reason='';
-    if (! await verifyCookie(req)) {
-        reason="Not logged in.";
-    }
-    else {
-        let user = await User.findOne({username: req.query.loginname});
-        if (! haveAccess(user, req.query.id)) {
-            reason="You don't have access to this article.";
+    try {
+        console.log("FETCH", req.query);
+        let article = await fetchArticle(req.query.id);
+        if(article==null) {
+            res.send({
+                reason: "Article not found.",
+            });
+            return ;
         }
+        let reason='';
+        if (! await verifyCookie(req)) {
+            reason="Not logged in.";
+        }
+        else {
+            let user = await User.findOne({username: req.query.loginname});
+            if (! haveAccess(user, req.query.id)) {
+                reason="You don't have access to this article.";
+            }
+        }
+        if(reason!='') article.content='';
+        res.send({
+            article: article.id,
+            title: article.title,
+            description: article.description,
+            content: article.content,
+            votes: article.votes,
+            author: article.author,
+            time: article.time,
+            type: article.type,
+            reason: reason,
+        });
+    } catch (err) {
+        console.log(err);
     }
-    if(reason!='') article.content='';
-    res.send({
-        article: article.id,
-        title: article.title,
-        description: article.description,
-        content: article.content,
-        votes: article.votes,
-        author: article.author,
-        time: article.time,
-        type: article.type,
-        reason: reason,
-    });
 });
 
 // /api/retrieve: 
@@ -275,39 +339,46 @@ app.get("/api/fetch", async (req, res) => {
 // ​		Output: Give you the access. 
 
 app.get("/api/retrieve", async (req, res) => {
-    if (! await verifyCookie(req)) {
-        res.send("Not logged in");
-        return ;
+    try {
+        if (! await verifyCookie(req)) {
+            res.send("Not logged in");
+            return ;
+        }
+        console.log("RETRIEVE", req.query);
+        let user = await User.findOne({username: req.query.username});
+        let receiver = await User.findOne({username: req.query.loginname});
+        if (haveAccess(receiver, req.query.article)) {
+            res.send("You already have access to this article");
+            return ;
+        }
+        if (! user) {
+            res.send("No such user");
+            return ;
+        }
+        let id = req.query.article;
+        if (CryptoJS.MD5(user.password + id) != req.query.hash) {
+            console.log("WRONG HASH");
+            res.send("Wrong hash");
+            return;
+        }
+        let history = getAccess(user, id);
+        if (history == null) {
+            res.send("The sender doesn't have access to this article");
+            return ;
+        }
+        if (history.shared != false) {
+            res.send("It has been shared");
+            return ;
+        }
+        history.shared = true;
+        user.save();
+        receiver.articles.push({article: id, cost: 0, shared: true});
+        receiver.save();
+        res.send("Success");
     }
-    let user = await User.findOne({username: req.query.username});
-    let receiver = await User.findOne({username: req.query.loginname});
-    if (haveAccess(receiver, req.query.article)) {
-        res.send("You already have access to this article");
-        return ;
+    catch (err) {
+        console.log(err);
     }
-    if (! user) {
-        res.send("No such user");
-        return ;
-    }
-    let id = req.query.article;
-    if (CryptoJS.MD5(user.password + id) != req.query.hash) {
-		res.send("Wrong hash");
-		return;
-	}
-    let history = getAccess(user, id);
-    if (history === null) {
-        res.send("The sender doesn't have access to this article");
-        return ;
-    }
-    if (history.shared) {
-        res.send("It has been shared");
-        return ;
-    }
-    history.shared = true;
-    user.save();
-    receiver.articles.push({article: id, cost: 0, shared: true});
-    receiver.save();
-    res.send("Success");
 });
 
 /**
@@ -315,37 +386,42 @@ app.get("/api/retrieve", async (req, res) => {
 ​		Input: req.query.vote: +-1, req.query.id: article to vote
  */
 app.get("/api/vote", async (req, res) => {
-    if (! await verifyCookie(req)) {
-        res.send("Not logged in");
-        return ;
+    try {
+        if (! await verifyCookie(req)) {
+            res.send("Not logged in");
+            return ;
+        }
+        let user = await User.findOne({username: req.query.loginname});
+        const history = getAccess(user, req.query.id);
+        if (!history)  {
+            res.send("You don't have access to this article");
+            return ;
+        }
+        if (history.cost >= 2) {
+            res.send("You have already voted");
+            return ;
+        }
+        if (history.cost < 0) {
+            res.send("You are the author");
+            return ;
+        }
+        const article = await fetchArticle(req.query.id);
+        if (req.query.vote == 1) 
+            article.votes.upvotes++;
+        else if (req.query.vote == -1) 
+            article.votes.downvotes++;
+        article.save();
+        if (req.query.vote == 1) 
+            chargeUser(article.author, -history.cost);
+        res.send(""+history.cost);
+        history.cost = 2;
+        if (req.query.vote == -1)
+            history.cost = 3;
+        user.save();
     }
-    let user = await User.findOne({username: req.query.loginname});
-    const history = getAccess(user, req.query.id);
-    if (!history)  {
-        res.send("You don't have access to this article");
-        return ;
-    }
-    if (history.cost >= 2) {
-        res.send("You have already voted");
-        return ;
-    }
-    if (history.cost < 0) {
-        res.send("You are the author");
-        return ;
-    }
-    const article = await fetchArticle(req.query.id);
-    if (req.query.vote == 1) 
-        article.votes.upvotes++;
-    else if (req.query.vote == -1) 
-        article.votes.downvotes++;
-    article.save();
-    if (req.query.vote == 1) 
-        chargeUser(article.author, -history.cost);
-    res.send(""+history.cost);
-    history.cost = 2;
-    if (req.query.vote == -1)
-        history.cost = 3;
-    user.save();
+    catch (err) {
+        console.log(err);
+    }   
 });
 
 
@@ -355,7 +431,12 @@ app.get("/api/vote", async (req, res) => {
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    try {
+        res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    }
+    catch (err) {
+        console.log(err);
+    }
 });
 
 
